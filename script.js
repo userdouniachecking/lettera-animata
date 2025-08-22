95041bb4af5aad75a1e934651dd0ba200935e91c
userdouniachecking <basilicata4f@gmail.com>
2025-08-22 12:30:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 const letter = document.querySelector('.letter');
-const message = document.querySelector('.letter-inside p');
 
 letter.addEventListener('click', () => {
     letter.classList.toggle('open');
-
-    if (letter.classList.contains('open')) {
-        message.style.opacity = 0;
-        setTimeout(() => {
-            message.style.transition = 'opacity 0.5s';
-            message.style.opacity = 1;
-        }, 600); // leggero ritardo per effetto sorpresa
-    } else {
-        message.style.opacity = 0;
-    }
 });
